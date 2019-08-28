6f06a8f76c5dccab2eaeec73d81039054183f6b0
James <jameskelly21699@gmail.com>
2019-08-28 10:59:57 +0100

    Mesh can rotate

--- OpenGL/Rendering/Mesh.cpp
+++ OpenGL/Rendering/Mesh.cpp
@@ -81,6 +81,9 @@ void Mesh::Draw(const Shader &shader, const int tex)
     
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, Transform->Position);
+    model = glm::rotate(model, glm::radians(Transform->Rotation.x), glm::vec3(1, 0, 0));
+    model = glm::rotate(model, glm::radians(Transform->Rotation.y), glm::vec3(0, 1, 0));
+    model = glm::rotate(model, glm::radians(Transform->Rotation.z), glm::vec3(0, 0, 1));
     model = glm::scale(model, Transform->Scale);
     glUniformMatrix4fv(glGetUniformLocation(shader.GetShaderID(), "model"), 1, GL_FALSE, &model[0][0]);
     
